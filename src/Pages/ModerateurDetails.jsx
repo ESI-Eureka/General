f440b6474ff3e0686f76a32386926755c3ca31a5
lylia5 <ll_aouinine@esi.dz>
2024-01-03 20:39:27 +0100

    correction

--- src/Pages/ModerateurDetails.jsx
+++ src/Pages/ModerateurDetails.jsx
@@ -1,5 +1,3 @@
-// ModerateurDetails.js
-
 import React, { useState, useEffect } from 'react';
 import NavBar from '../Components/NavBar';
 import ResultatDetails from '../Components/ResultatDetails';
@@ -11,62 +9,64 @@ import { ReactComponent as Save } from '../Icons/Save.svg';
 import IconedButton from '../Components/IconedButton';
 import { Link, useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+
 const ModerateurDetails = () => {
   const navItems = [
     { text: "Articles", path: "/article", className: "ModerateurDetails" },
     { text: "Profile", path: "/profil", className: "Profile" },
   ];
 
-  const [data, setData] = useState({
-    auteurs: "Auteurs",
-    institutions: "Institutions",
-    mots_cles: "Mots_cles",
-    pdf_url: "pdf_url",
-    publication_date: "publication_date",
-    references: "references",
-    resume: "resume",
-    texte_integral: "texte_integral",
-    titre: "titre",
-  });
-
+  const location = useLocation();
+  const [initialData, setInitialData] = useState(location.state?.data);
+  const [data, setData] = useState(location.state?.data);
   const [editMode, setEditMode] = useState(false);
 
-  const location = useLocation();
-
   useEffect(() => {
+    setInitialData(location.state?.data);
     setData(location.state?.data);
   }, [location.state?.data]);
 
   const handleEditClick = () => {
     setEditMode(!editMode);
-    
   };
+
+  const handleSaveClick = () => {
+    // Perform save logic here
+    setEditMode(false);
+    setInitialData(data);
+  };
+
+  const handleCancelClick = () => {
+    // Revert back to the initial data on cancel
+    setData(initialData);
+    setEditMode(false);
+  };
+
   const navigate = useNavigate();
-const handleReturn = () => {
+  const handleReturn = () => {
     navigate(-1);
   };
+
   return (
     <div>
       <NavBar navItems={navItems} />
       <div className="DetailsContainer">
         <div className="NavCorriger">
-          
-            <RightFleche onClick={handleReturn} />
+          <RightFleche onClick={handleReturn} />
           {!editMode ? (
-            
-          <IconedButton icon={Ecrire} text="Correct" onClick={handleEditClick} />):
-          (<><IconedButton icon={Save} text="Save" onClick={handleEditClick} />
-          <span className='cancle'>Cancel</span>
-          </>
-          
+            <IconedButton icon={Ecrire} text="Correct" onClick={handleEditClick} />
+          ) : (
+            <div className='correction'>
+              <IconedButton icon={Save} text="Save" onClick={handleSaveClick} />
+              <span className='cancel' onClick={handleCancelClick}>Cancel</span>
+            </div>
           )}
-
         </div>
         <div className="ResultatDetailsContainer">
           <ResultatDetails
             data={data}
             setData={setData}
-              editMode={editMode}
+            editMode={editMode}
           />
 
           <div className="moreDetails">
